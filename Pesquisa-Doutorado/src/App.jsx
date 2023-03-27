22ffcc04f9684b26207fbd8b8bec30dc1fbc003a
nyk <nykcasseano13@gmail.com>
2023-03-26 23:15:54 -0300

    Versão incial  do código que corresponde ao primeiro dia de trabalho

--- Pesquisa-Doutorado/src/App.jsx
+++ Pesquisa-Doutorado/src/App.jsx
@@ -3,33 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/*firebase code part*/
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLJfCkfrL2LL1nbuFOiaId8vBEscGAS68",
+  authDomain: "pesquisa-doutorado.firebaseapp.com",
+  projectId: "pesquisa-doutorado",
+  storageBucket: "pesquisa-doutorado.appspot.com",
+  messagingSenderId: "483890775800",
+  appId: "1:483890775800:web:fb81c3262e3dbecfc79d0d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const App = () => {
+  const [profissao, setProfissao] = useState("");
+  const [nivelSuper, setnivelSuper] = useState("");
+  const [curso, setCurso] = useState("");
+  const [estado, setEstado] = useState("");
+  const [sexo, setSexo] = useState("")
+  const [nasc, setNasc] = useState("");
+  const [medico, setMedico] = useState("");
+  const [especial, setEspecial] = useState("");
+  const [cpf, setCpf] = useState("");
+
+  const db = getFirestore(firebaseApp);
+  const userCollectionRef = collectionRef(db, "users");
+
+  useEffect(() => {
+    const getUsers = async (users) => {
+      const data = await getDocs()
+    }
+  },[])
+
 }
-
-export default App
